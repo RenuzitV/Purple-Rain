bc8ed09e0dd7a881878fd147319789e0fa4cad32
RenuzitV <nvminhduy2002@gmail.com>
2021-12-22 18:47:13 +0700

    update code

--- drop.js
+++ drop.js
@@ -1,38 +1,47 @@
 function Drop() {
+  this.reYSpeed = function(){
+    return map(this.z, 0, 20, 10, 15);
+  }
+  this.reXSpeed = function(){
+    return random(-0.03, 0.03);
+  }
+
   this.x = random(width);
-  this.y = random(-200, -50);
+  this.y = random(-width, 0);
   this.z = random(0, 20);
   this.len = map(this.z, 0, 20, 8, 20);
-  this.yspeed = map(this.z, 0, 20, 1, 10);
-  this.xspeed = random(-0.5, 0.5);
+  this.yspeed = this.reYSpeed();
+  this.xspeed = this.reXSpeed();
   this.maxX = 3;
+  this.thick = map(this.z, 0, 20, 1, 2.5);
+
 
   this.fall = function() {
     this.y = this.y + this.yspeed;
     this.x = this.x + this.xspeed;
 //     var grav = map(this.z, 0, 20, 0, 0.01);
     this.yspeed = max(min(this.yspeed + grav, 10), -10);
-    this.xspeed = max(min(this.xspeed + windspeed, this.maxX), -this.maxX);
+    // this.xspeed = max(min(this.xspeed + windspeed, this.maxX), -this.maxX);
+    this.xspeed = map(mouseX, 0, width, -1, 1);
 
     if (this.y > height + 50) {
       this.y = random(-200, -50);
-      this.yspeed = map(this.z, 0, 20, 0.5, 5);
+      this.yspeed = this.reYSpeed();
+      this.xspeed = this.reXSpeed();
     }
-    if (this.y < -200) {
-      this.y = random(height, height+50);
-    }
+
     if (this.x > width+10) this.x = random(-5, 0);
     else if (this.x < 0) this.x = random(width, width+5);
   }
+
   this.show = function() {
-    var thick = map(this.z, 0, 20, 1, 2.5);
-    strokeWeight(thick);
+    strokeWeight(this.thick);
     stroke(138, 43, 226);
     mx = map(this.xspeed, -this.maxX, this.maxX, -2, 2);
     my = map(this.yspeed, -10, 10, -2, 2);
-    shift = map(mx*my, -2, 4, -1, 1);
-    x1 = this.x-shift;
-    x2 = this.x+shift;
+    // shift = map(mx*my, -4, 4, -1, 1);
+    x1 = this.x-this.xspeed;
+    x2 = this.x+this.xspeed;
 //     if (mouseY < height/2) [x1, x2] = [x2, x1];
     line(x1, this.y, x2, this.y+this.len);
     fill(138, 43, 226);
--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 var drops = [];
 var windspeed = 0;
-var grav = 0.01;
+var grav = 0.02;
 function setup() {
-  createCanvas(800, 600);
-  for (var i = 0; i < 100; i++) {
+  createCanvas(1024, 576);
+  for (var i = 0; i < 200; i++) {
     drops[i] = new Drop();
   }
   
@@ -11,8 +11,8 @@ function setup() {
 
 function draw() {
   background(230, 230, 250);
-  windspeed = map(mouseX, 0, width, -0.03, 0.03);
-  grav = map(mouseY, 0, height, -0.05, 0.05);
+  windspeed = map(mouseX, 0, width, -0.05, 0.05);
+  // grav = map(mouseY, 0, height, -0.05, 0.05);
 //   windspeed += random(-0.1, 0.1);
   for (var i = 0; i < drops.length; i++) {
     drops[i].fall();
